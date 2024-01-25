184fc27a0a95aafcb2c027e1f2d21bef02388d34
Maria Pallarés <mpallareslara@gmail.com>
2024-01-25 15:14:52 +0000

    Bootstrap set up and routing

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Store } from './pages/Store';
+
+export const App = () => {
+  return (
+    <Container>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Container>
+  );
+};
