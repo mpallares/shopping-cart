2b36a1d401c70f6974de5310d7925e0963172f04
Maria Pallarés <mpallareslara@gmail.com>
2024-02-02 15:16:08 +0000

    store showing final touches

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import { Container } from 'react-bootstrap';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Store } from './pages/Store';
+import { Navbar } from './components/Navbar';
+import { ShoppingCartProvider } from './context/shoppingCartContext';
 
 export const App = () => {
   return (
-    <Container>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/store' element={<Store />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </Container>
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/store' element={<Store />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   );
 };
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import { StoreItem } from '../components/StoreItem';
+import storeItems from '../data/items.json';
+import { Col, Row } from 'react-bootstrap';
 
 export const Store = () => {
-  return <div>Store</div>;
+  return (
+    <>
+      <div>Store</div>
+      <Row md={2} xs={1} lg={3} className='g-3'>
+        {storeItems.map((item) => {
+          return (
+            <Col key={item.id}>
+              <StoreItem {...item} />
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
 };
